64346a4e581316fb31928dee028205e7e2825a70
jmkim <isugar150@gmail.com>
2026-02-10 10:26:22 +0900

    로직 개선

--- bin/startup.bat
+++ bin/startup.bat
@@ -1,7 +1,12 @@
 @echo off
 
 title JmProxyServer
-set "BASE_DIR=%~dp0.."
+set "SCRIPT_DIR=%~dp0"
+for %%I in ("%SCRIPT_DIR%.") do set "SCRIPT_DIR=%%~fI"
+set "BASE_DIR=%SCRIPT_DIR%"
+if not exist "%BASE_DIR%\JmProxyServer.jar" (
+    for %%I in ("%SCRIPT_DIR%..") do set "BASE_DIR=%%~fI"
+)
 set "CONFIG_PATH=%BASE_DIR%\config\application.yml"
 set "LOGBACK_CONFIG=%BASE_DIR%\config\logback.xml"
 java -server -Xms512M -Xmx1024M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8 -Dlogback.configurationFile="%LOGBACK_CONFIG%" -jar "%BASE_DIR%\JmProxyServer.jar" "%CONFIG_PATH%"
